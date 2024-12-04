d49b8a76888fd02eb4f947ba5cd0eefc95779096
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-12-04 11:47:42 +0100

    basic configuration to start using graphql
    -use "npm i @nestjs/graphql @nestjs/apollo @apollo/server graphql"
    -add data in app.module
    -check next step to fix the issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
