bc6c968c9048448f43ffd74104f00f7b91c135b5
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-12-09 14:58:09 +0100

    add resolver

--- src/hello-world/hello-world.module.ts
+++ src/hello-world/hello-world.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { HelloWorldResolver } from './hello-world.resolver';
+import { Query } from '@nestjs/graphql';
 
-@Module({})
-export class HelloWorldModule {}
+@Module({
+  providers: [HelloWorldResolver]
+})
+export class HelloWorldModule {
+  @Query(() => String)
+  helloWorld(): string {
+    return 'Hello World!';
+  }
+}
