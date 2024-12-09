9897a744e416c691dd1da809c581bc4f86d5cff4
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-12-09 17:40:20 +0100

    preparation next usage

--- src/hello-world/hello-world.module.ts
+++ src/hello-world/hello-world.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { HelloWorldResolver } from './hello-world.resolver';
-import { Float, Query } from '@nestjs/graphql';
+import { Float, Int, Query } from '@nestjs/graphql';
 
 @Module({
   providers: [HelloWorldResolver]
@@ -17,5 +17,10 @@ export class HelloWorldModule {
   @Query(() => Float, {name: 'randomNumber'})
   getRandomNumber(): number {
     return Math.random() * 100;
+  }
+
+  @Query(() => Int, {name: 'randomFromZeroTo'})
+  getRandomFromZeroTo(): number {
+    return Math.floor(Math.random() * 10);
   }
 }
