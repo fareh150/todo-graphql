08ba9296fe364b741f5a9ff4ac0d9fae05b8227e
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-12-09 13:59:41 +0100

    generate new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { HelloWorldModule } from './hello-world/hello-world.module';
+import { HelloWorldResolver } from './—no-spec/hello-world/hello-world.resolver';
 
 @Module({
   imports: [
@@ -9,8 +11,9 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    HelloWorldModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [HelloWorldResolver],
 })
 export class AppModule {}
