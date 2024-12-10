8faca5d002d8ec405e90e59f9146b155f4503cd6
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-12-10 12:12:52 +0100

    manage create todo

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Todo } from './entity/todo.entity';
+import { CreateTodoInput } from './dto/inputs/create-todo.input';
 
 @Injectable()
 export class TodoService {
@@ -48,6 +49,16 @@ export class TodoService {
         {
             throw new NotFoundException(`Todo with id ${id} not found`);
         }
+        return todo;
+    }
+
+    create(createTodoInput: CreateTodoInput): Todo {
+        const todo = new Todo();
+        todo.id = Math.max(...this.todos.map(todo => todo.id), 0) + 1;
+        todo.description = createTodoInput.description;
+
+        this.todos.push(todo);
+
         return todo;
     }
 }
