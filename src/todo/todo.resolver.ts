0e68b02eb6c149739c22b08512fa261b1b40eee0
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-12-10 11:05:59 +0100

    use service

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,11 +1,16 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { Todo } from './entity/todo.entity';
+import { TodoService } from './todo.service';
 
 @Resolver()
 export class TodoResolver {
+    constructor(
+        private readonly todoService: TodoService
+    ) {}
+
     @Query(() => [Todo], {name: 'todos'})
     findAll(): Todo[] {
-        return [];
+        return this.todoService.findAll();
     }
 
     findOne(){
